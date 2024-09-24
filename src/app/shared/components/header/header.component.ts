43e682f1cc582024c83744154656c20f5bd11b91
Lucas <lucas.gomes@timesharesolucoes.com.br>
2024-09-24 15:11:09 -0300

    Adicionado: componente header

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  public opened = false;
+}
