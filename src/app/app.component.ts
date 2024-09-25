76bb3502314c33bbb92535c21bb222cd163341d6
Lucas <lucas.gomes@timesharesolucoes.com.br>
2024-09-25 17:49:03 -0300

    Modificado: ajustes gerais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
-import { LoginComponent } from "./pages/login/login.component";
-import { FooterComponent } from "./pages/footer/footer.component";
-import { RegisterComponent } from "./pages/register/register.component";
+import { FooterComponent } from './pages/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LoginComponent, FooterComponent, RegisterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
