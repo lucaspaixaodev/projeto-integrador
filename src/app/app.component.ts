62f863bf74e07172852698badc69548cebd10d0a
Lucas <lucas.gomes@timesharesolucoes.com.br>
2024-09-25 16:08:28 -0300

    Modificado: organização do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from "./login/login.component";
+import { HeaderComponent } from './pages/header/header.component';
+import { LoginComponent } from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
