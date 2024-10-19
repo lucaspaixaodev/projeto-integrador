a37e035ab1b860af0297d5e339e79d816637e9e5
Lucas Paixao <lucasgpaixao_dev@hotmail.com>
2024-10-19 18:22:29 -0300

    Modificado: descrição dos produtos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -18,13 +18,13 @@ export class HomeComponent {
   cartService = inject(CartService);
 
   public products = [
-    { id: 1, name: 'Cupcake de Chocolate', price: 9.99, image: 'assets/images/cupcake-chocolate.jpg' },
-    { id: 2, name: 'Cupcake de Baunilha', price: 8.99, image: 'assets/images/cupcake-baunilha.jpeg' },
-    { id: 3, name: 'Cupcake de Morango', price: 10.99, image: 'assets/images/cupcake-morango.png' },
-    { id: 4, name: 'Cupcake de Limão', price: 9.49, image: 'assets/images/cupcake-limao.jpg' },
-    { id: 5, name: 'Cupcake de Cenoura', price: 8.99, image: 'assets/images/cupcake-cenoura.jpg' },
-    { id: 6, name: 'Cupcake de Frutas Vermelhas', price: 11.99, image: 'assets/images/cupcake-frutas-vermelhas.jpg' },
-    { id: 7, name: 'Cupcake de Caramelo', price: 10.49, image: 'assets/images/cupcake-caramelo.jpg' }
+    { id: 1, name: 'Cupcake de Chocolate', price: 9.99, image: 'assets/images/cupcake-chocolate.jpg', description: 'Delicioso cupcake de chocolate rico e cremoso, coberto com ganache de chocolate belga e raspas de chocolate amargo.' },
+    { id: 2, name: 'Cupcake de Baunilha', price: 8.99, image: 'assets/images/cupcake-baunilha.jpeg', description: 'Cupcake clássico de baunilha com massa macia e aroma irresistível, decorado com buttercream de baunilha e confeitos coloridos.' },
+    { id: 3, name: 'Cupcake de Morango', price: 10.99, image: 'assets/images/cupcake-morango.png', description: 'Cupcake fresco de morango com pedaços de fruta na massa, coberto com chantilly de morango e decorado com uma morango fresco.' },
+    { id: 4, name: 'Cupcake de Limão', price: 9.49, image: 'assets/images/cupcake-limao.jpg', description: 'Cupcake cítrico de limão siciliano, com cobertura de merengue italiano e raspas de limão caramelizadas.' },
+    { id: 5, name: 'Cupcake de Cenoura', price: 8.99, image: 'assets/images/cupcake-cenoura.jpg', description: 'Cupcake úmido de cenoura com especiarias, coberto com frosting de cream cheese e decorado com minicenouras de açúcar.' },
+    { id: 6, name: 'Cupcake de Frutas Vermelhas', price: 11.99, image: 'assets/images/cupcake-frutas-vermelhas.jpg', description: 'Cupcake recheado com mix de frutas vermelhas, coberto com mousse de frutas vermelhas e decorado com frutas frescas.' },
+    { id: 7, name: 'Cupcake de Caramelo', price: 10.49, image: 'assets/images/cupcake-caramelo.jpg', description: 'Cupcake de caramelo salgado com núcleo de caramelo líquido, coberto com buttercream de caramelo e decorado com flocos de sal marinho.' }
   ];
 
   constructor() {
