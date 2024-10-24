35cd24071de04d97f5e4f395524931ddf5d98b26
Lucas <lucaspaixao_dev@hotmail.com>
2024-10-24 14:18:42 -0300

    Modificado: server

--- cupcake-backend/server.js
+++ cupcake-backend/server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
 // Conectar ao banco de dados
